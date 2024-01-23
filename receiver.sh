2aa66f05f78c965333a3732a805dcaa2428b93e7
Hassna Fattah <119518826+hecrinii@users.noreply.github.com>
2024-01-23 09:36:51 +0100

    Update receiver.sh

--- receiver.sh
+++ receiver.sh
@@ -5,9 +5,4 @@ echo "WELCOME TO THE ITS LAST CHAT!"
 
 chatlog='chat.log'
 
-while [[ true ]]
-do
-	nc -l -b -u 4444 | tee -a $chatlog
-	#equivalent to:
-	# nc -l 4444 >> $chatlog
-done
+nc -luk 4444 | tee -a $chatlog
